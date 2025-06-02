f38282918deb82e55eaa2630b1ac21de12ba993a
anicperica <anicperica18@gmail.com>
2025-06-02 17:05:35 +0200

    Add fetch and display of books

--- src/components/DiscoverContent.tsx
+++ src/components/DiscoverContent.tsx
@@ -4,7 +4,15 @@ import DiscoverButton from "./ui/DiscoverButton";
 
 const LIMIT_ARRAY = [1, 2, 3];
 
-export default function DiscoverContent() {
+export default function DiscoverContent({
+  queryLimit,
+  setQueryLimit,
+  onRefresh,
+}: {
+  queryLimit: number;
+  setQueryLimit: (n: number) => void;
+  onRefresh: () => void;
+}) {
   return (
     <div className="flex flex-col justify-center items-center py-15 bg-amber-100">
       <BookIcon width={50} height={50} color="#b66b02" />
@@ -16,11 +24,22 @@ export default function DiscoverContent() {
       <div className="flex  justify-center items-center gap-5 py-5 ">
         <p>Show:</p>
         {LIMIT_ARRAY.map((limit) => (
-          <DiscoverButton key={limit}>{limit}</DiscoverButton>
+          <DiscoverButton
+            key={limit}
+            onClick={() => setQueryLimit(limit)}
+            className={
+              queryLimit === limit ? "bg-primary text-black" : "bg-white"
+            }
+          >
+            {limit}
+          </DiscoverButton>
         ))}
         <p>Books</p>
       </div>
-      <button className="flex justify-center items-center bg-primary px-5 py-3 rounded-xl gap-2 cursor-pointer">
+      <button
+        onClick={onRefresh}
+        className="flex justify-center items-center bg-primary px-5 py-3 rounded-xl gap-2 cursor-pointer hover:bg-amber-800"
+      >
         <RefreshIcon width={20} height={20} color="black" />
         Refresh Recommendations
       </button>
--- src/pages/DiscoveryPage/DiscoverPage.tsx
+++ src/pages/DiscoveryPage/DiscoverPage.tsx
@@ -1,16 +1,25 @@
+import { useState } from "react";
 import Navbar from "../../components/Navbar";
 import DiscoverContent from "../../components/DiscoverContent";
-import BookCard from "../../components/BookCard";
+import BookList from "../../components/BookList";
 export default function DiscoverPage() {
+  const [queryLimit, setQueryLimit] = useState(3);
+  const [refreshKey, setRefreshKey] = useState(0);
+
+  const handleRefresh = () => setRefreshKey((key) => key + 1);
+
   return (
     <>
       <div className="bg-amber-50">
         <Navbar />
-        <DiscoverContent />
-
+        <DiscoverContent
+          queryLimit={queryLimit}
+          setQueryLimit={setQueryLimit}
+          onRefresh={handleRefresh}
+        />
         <h1 className="text-2xl p-10">Recomended for you</h1>
         <div className="flex flex-row gap-10 justify-center">
-          <BookCard title="War and peace" author="By Lav tolstoj" />
+          <BookList limit={queryLimit} refreshkey={refreshKey} />
         </div>
       </div>
     </>
